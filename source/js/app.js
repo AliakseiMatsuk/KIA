558bee1dd89cd2d4e4430134386e7d4cc469ffb9
Aliaksei <matsukaliaksei@gmail.com>
2017-06-23 14:12:23 +0300

    add map, contacts

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,42 @@
 (function() {
-  'use strict';
+    'use strict';
+
+    ymaps.ready(initMap);
+    var myMap,
+        myPlacemark;
+
+    function initMap() {
+        myMap = new ymaps.Map("map", {
+            center: [53.953479, 27.717943],
+            zoom: 16,
+            controls: ['fullscreenControl']
+        });
+
+        myPlacemark = new ymaps.Placemark([53.953479, 27.717943], {
+            balloonContent: 'Автоцентр KIA Атлант-м на Независимости Автопалас-м'
+        },{
+            iconLayout: 'default#image',
+            iconImageClipRect: [[0,0], [26, 47]],
+            iconImageHref: 'templates/_ares/images/sprite-img.png',
+            iconImageSize: [75, 53],
+            iconImageOffset: [-20, -55]
+        });
+
+        myMap.geoObjects.add(myPlacemark);
+    }
+
+
+
+
+        $(window).scroll(function(){
+        	 var $buttons = $(".btn-round");
+        	//console.log($(window).scrollTop());
+            if ( $(this).scrollTop() > 700 && $buttons.hasClass("default") ){
+                $buttons.removeClass("default").addClass("fixed");
+            } else if($(this).scrollTop() <= 700 && $buttons.hasClass("fixed")) {
+                $buttons.removeClass("fixed").addClass("default");
+            }
+        });
 })();
+
+
